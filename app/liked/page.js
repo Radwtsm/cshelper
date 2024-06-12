7a4a21c78121bf2882d7e557954fe6c8cac1490d
rathushan.ramesh <rathushan.ramesh@txgroup.com>
2024-06-12 12:35:44 +0200

    undefined localstorage

--- app/liked/page.js
+++ app/liked/page.js
@@ -5,11 +5,12 @@ import TipCard from '../TipCard'
 import { Button, Typography } from '@mui/material'
 
 const Page = () => {
-  const [likedTips, setLikedTips] = useState(JSON.parse(localStorage.getItem('cs2smokes_fav')))
+  const [likedTips, setLikedTips] = useState(global?.window != undefined ? JSON.parse(localStorage.getItem('cs2smokes_fav')) : [])
 
   function onResetFav(){
     setLikedTips([])
-    localStorage.setItem('cs2smokes_fav',JSON.stringify([]))
+    global?.window != undefined ? localStorage.setItem('cs2smokes_fav',JSON.stringify([])) : null
+    
     
   }
 
@@ -17,6 +18,7 @@ const Page = () => {
     <>    
     <Typography sx={{color:'white',my:4}} variant="h5" gutterBottom>
         LIKED TRICKS
+        
       </Typography>
     <div className=" flex flex-col gap-5 my-2">
         {likedTips.length > 0 ? likedTips.map(el=><TipCard key={el.title} el={el}/>) : <p>No elements</p>}
